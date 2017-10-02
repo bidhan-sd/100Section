b46bc76883edb826688674a000554ab3de366386
Bidhan sutradhar <bidhanvk@gmail.com>
2017-10-02 23:46:37 +0600

    class 03 down here

---
+++ Class-03/class03/js/main.js
@@ -0,0 +1,24 @@
+(function($){
+	
+	"use strict";
+	jQuery(document).ready(function($){
+		
+		$(video-play-btn).magnificPopup({
+			
+			type:'video',
+			
+		});
+		
+		
+		
+	});
+	
+	
+	jQuery(document).load(function(){
+		
+		
+		
+	});
+	
+	
+}(jQuery));
