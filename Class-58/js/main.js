91dc34734f60c92c2a2bde5eb23261ceefb345bf
imbidhan <bidhanvk@gmail.com>
2018-04-05 22:34:11 +0600

    class js file added here

---
+++ Class-58/js/main.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($){
+    
+  $(".project-filter li").on('click',function(){
+      
+      var selector = $(this).attr("data-filter");
+       $(".project-list").isotope({
+           
+           filter: selector,
+       }); 
+      
+  });
+                       
+  $(".project-list").isotope(); 
+});
