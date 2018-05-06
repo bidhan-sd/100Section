ac2fe86870e9459282c86d35696dbb3c50a714da
imbidhan <bidhanvk@gmail.com>
2018-05-06 19:32:38 +0600

    class 63 js file added here

--- Class-03/js/main.js
+++ Class-03/js/main.js
@@ -1,24 +1,13 @@
 (function($){
 	
 	"use strict";
-	jQuery(document).ready(function($){
-		
-		$(video-play-btn).magnificPopup({
-			
-			type:'video',
-			
-		});
-		
-		
-		
+	jQuery(document).ready(function($){		
+		$(".video-play-btn").magnificPopup({type:'video'});		
 	});
 	
 	
-	jQuery(document).load(function(){
-		
-		
+	jQuery(document).load(function(){		
 		
 	});
-	
 	
 }(jQuery));
