74a6cbd373eb64ee671bf952274480cd1e5fc206
imbidhan <bidhanvk@gmail.com>
2018-05-16 23:27:40 +0600

    class 64 js added here

---
+++ Class-64/js/main.js
@@ -0,0 +1,24 @@
+(function($){
+	
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		
+	$(".counter-number span").counterUp({
+		 delay: 10,
+         time: 1000
+
+
+
+	});	
+		
+	
+});
+
+jQuery(window).load(function(){
+	
+	
+	
+});
+
+}(jQuery));
